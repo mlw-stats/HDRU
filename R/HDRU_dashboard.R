6a9ad113c1c4624b7da2a8740ef159cbe0049d38
Marc Henrion <henrion.marc@gmail.com>
2020-09-10 15:30:19 +0200

    adding an example for the man files

--- R/HDRU_dashboard.R
+++ R/HDRU_dashboard.R
@@ -16,6 +16,23 @@
 #' @importFrom dplyr filter summarise group_by mutate 
 #' @importFrom stats approxfun
 #' 
+#' @examples
+#' # You will need to
+#' #  * Load the admission data from the data portal (argument admissionData).
+#' #  * Load the daily data from the data portal (argument dailyData).
+#' #  * Set the weeks for which you want summaries (argument curWeek).
+#' #  * Specify a file name (with .html extension) that will contain the dashboard report.
+#' # The example below assumes you've got the 2 input data files sitting in your working directory.
+#' # Once you have run the HDRUdashboard() function, open the specified output html file.
+#' 
+#' library(HDRU)
+#' 
+#' admissionData<-read.csv("hdru_admission_raw.csv")
+#' dailyData<-read.csv("hdru_daily_raw.csv")
+#' curWeek<-dmy("06/07/2020","13/07/2020","20/07/2020","27/07/2020")
+#' 
+# HDRUdashboard(admissionData=admissionData,dailyData=dailyData,curWeek=curWeek,file.name="HDRUdashboard.html")
+#' 
 #' @export HDRUdashboard
 
 HDRUdashboard<-function(admissionData,dailyData,curWeek,file.name){
